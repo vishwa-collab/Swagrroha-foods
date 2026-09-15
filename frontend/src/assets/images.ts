bfe0f6a239b418c532dc25d50cfaefe53fdc1864
vishwa-collab <vishwa81251@gmail.com>
2026-09-15 05:50:20 +0530

    commit

--- frontend/src/assets/images.ts
+++ frontend/src/assets/images.ts
@@ -1,7 +1,7 @@
 // src/assets/images.ts
 // Direct imports so Vite hashes & bundles each image correctly.
 
-import logo from '../../assets/logo of pjr swagrooha foods.jpeg';
+import logo from '../../assets/logo.png';
 import murukulu from '../../assets/Murkullu hot item.png';
 import redSakinalu from '../../assets/Red Sakinnalu hot item.png';
 import yellowSakinalu from '../../assets/yellow sakinnalu hot item.png';
